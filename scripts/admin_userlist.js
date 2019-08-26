b7c9b11632743bb1d9691a53c492ba04a4fb2ddd
emadsi <emad.silawi89@gmail.com>
2019-08-26 21:25:07 +0300

    uppercase correction

--- scripts/admin_userlist.js
+++ scripts/admin_userlist.js
@@ -7,7 +7,7 @@ const tbody = document.querySelector('#tbody');
 for(const user in USERS){
     const empID = USERS[user].employeeNumber,
       empName = USERS[user].firstName.concat(' ', USERS[user].lastName),
-      empRole = USERS[user].Roles,
+      empRole = USERS[user].roles,
       empDep = USERS[user].department,
       empWorkSite = USERS[user].workSite;
 
