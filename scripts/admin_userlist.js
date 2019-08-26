accc04f037e9777665ff0c5c4979090fc51df2c3
emadsi <emad.silawi89@gmail.com>
2019-08-26 21:05:56 +0300

    Parsing dynamicly

--- scripts/admin_userlist.js
+++ scripts/admin_userlist.js
@@ -1,3 +1,17 @@
 // Material Select
-$('.mdb-select').materialSelect({
+/*$('.mdb-select').materialSelect({
 });
+*/
+const tbody = document.querySelector('#tbody');
+
+for(const user in USERS){
+    const empID = USERS[user].employeeNumber,
+      empName = USERS[user].firstName.concat(' ', USERS[user].lastName),
+      empRole = USERS[user].Roles,
+      empDep = USERS[user].department,
+      empWorkSite = USERS[user].workSite;
+
+    tbody.innerHTML += `<tr><td>${empID}</td><td>${empName}</td>
+                    <td>${empRole}</td><td>${empDep}</td><td>${empWorkSite}</td></tr>`;
+}
+
