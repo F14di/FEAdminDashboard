a361d75676f170c892cc50e4b80b8c80f5bd982a
Fadi Muhammad <ffadi14@gmail.com>
2019-08-26 13:51:31 +0300

    added mock data

---
+++ data.js
@@ -0,0 +1,68 @@
+
+const loggedInUser={
+	userId:,
+	name:""
+};
+
+const USER={
+	employeeId:"",
+	firstName:"",
+	lastName:"",
+	managerName:"",
+	managerId:,
+	workSite:"",
+	lastLogin:"",
+	department:,
+	email:"",
+	phone:"",
+	roles:[
+			{roleID:   , 
+			roleName:""}
+			],
+};
+
+const USERS=[
+{
+	employeeNumber:,
+	firstName:"",
+	lastName:"",
+	roles:"",
+	department:"",
+	workSite:""
+},{
+	employeeNumber:,
+	firstName:"",
+	lastName:"",
+	roles:"",
+	department:"",
+	workSite:""
+},{
+	employeeNumber:,
+	firstName:"",
+	lastName:"",
+	roles:"",
+	department:"",
+	workSite:""
+},{
+	employeeNumber:,
+	firstName:"",
+	lastName:"",
+	roles:"",
+	department:"",
+	workSite:""
+},{
+	employeeNumber:,
+	firstName:"",
+	lastName:"",
+	roles:"",
+	department:"",
+	workSite:""
+},{
+	employeeNumber:,
+	firstName:"",
+	lastName:"",
+	roles:"",
+	department:"",
+	workSite:""
+},
+]
