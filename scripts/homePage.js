93104daafbab428db95f15363e270f10126942ab
emadsi <emad.silawi89@gmail.com>
2019-09-03 02:27:23 +0300

    setting some changes in the pages

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -19,3 +19,14 @@ for(const user in USERS){
     tbody.innerHTML += `<tr><td>${empID}</td><td>${empName}</td>
                     <td>${empRole}</td><td>${empDep}</td><td>${empWorkSite}</td></tr>`;
 }
+
+function showOptions(){
+  const advancedSearch = document.querySelector('#search-users-select');
+
+  if (advancedSearch.style.display == "flex") {
+    advancedSearch.style.display = "none";
+  }
+  else {
+    advancedSearch.style.display = "flex";
+  }
+}
