bb80e4a4619fd789d701d886b4d44e4535603e98
fadi <ffadi14@gmail.com>
2019-09-02 21:32:52 +0300

    fixed navar bugs

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -1,8 +1,8 @@
+function ToggleNavbar(){
+    document.getElementById('navBar').classList.toggle('toggleNav')
+    document.getElementById('body').classList.toggle('toggleMain')
 
-    function toggleNav() {
-        document.getElementById("sideNavBar").classList.toggle('widthToggle');
-        document.getElementById("main").classList.toggle('marginToggle');
-        document.querySelectorAll(".navbar__path").forEach(el=>el.classList.toggle('hamburgerPath'));
-        document.querySelectorAll(".nav__ul li a p").forEach(el=>el.classList.toggle('hideContent'));
+    document.querySelectorAll(".navbar__path").forEach(el=>el.classList.toggle('hamburgerPath'));
+   
+}
 
-        }
