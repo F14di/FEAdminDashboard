0a65eb04642bc57dcef6f92ec80cb73fbbf6dafa
Fadi Muhammad <ffadi14@gmail.com>
2019-09-04 11:21:55 +0300

    fixed function syntax

--- api-functions.js
+++ api-functions.js
@@ -8,61 +8,62 @@ const dataURL={
 	roles:'roles'
 }
 
-async function getData=(url,id)=>{
-	if (id){
-		return await fetch(`www.emp-amdocs.com/${dataURL[url]}/${id}`)
-					.then(res=>res.json())
-	}
-	return await fetch(`www.emp-amdocs.com/${dataURL[url]}`)
-					.then(res=>res.json())
+// async function getData=(url,id)=>{
+// 	if (id){
+// 		return await fetch(`www.emp-amdocs.com/${dataURL[url]}/${id}`)
+// 					.then(res=>res.json())
+// 	}
+// 	return await fetch(`www.emp-amdocs.com/${dataURL[url]}`)
+// 					.then(res=>res.json())
+// }
+
+const validateUserLogin= async ()=>(username, password){
+	// loginResponse stores admin details in case of success. 
+	//userValidated = false in case of wrong input
+	const loginResponse= await fetch('www.emp-amdocs.com/{username}/{password}')
+								.then(res=>res.json())
+
+	return loginResponse;
 }
 
-// async function validateUserLogin(username, password){
-// 	// loginResponse stores admin details in case of success. 
-// 	//userValidated = false in case of wrong input
-// 	const loginResponse= await fetch('www.emp-amdocs.com/{username}/{password}')
-// 								.then(res=>res.json())
+const getUserList = async () => {
+	const userList = await fetch('www.emp-amdocs.com/admin/users')
+							.then(res=>res.json())
+	return userList;
+}
 
-// 	return loginResponse;
-// }
+const getDepartments = async ()=>{
+	const amdocsDepartments = await fetch('www.emp-amdocs.com/admin/departments')
+							.then(res=>res.json())
+	return amdocsDepartments;
+}
 
-// async function getUserList = ()=>{
-// 	const userList = await fetch('www.emp-amdocs.com/admin/users')
-// 							.then(res=>res.json())
-// 	return userList;
-// }
-
-// async function getDepartments =()=>{
-// 	const amdocsDepartments = await fetch('www.emp-amdocs.com/admin/departments')
-// 							.then(res=>res.json())
-// 	return amdocsDepartments;
-// }
-
-// async function getCountries =()=>{
+// const getCountries = async()=>{
 // 	const amdocsCountries = await fetch('www.emp-amdocs.com/admin/amdocsCountries')
 // 							.then(res=>res.json())
 // 	return amdocsCountries;
 // }
-// async function getWorkSites =()=>{
+
+// const getWorkSites = async()=>{
 // 	const amdocsWorkSites = await fetch('www.emp-amdocs.com/admin/amdocs_work_sites')
 // 							.then(res=>res.json())
 // 	return amdocsWorkSites;
 // }
 
-// async function getRoles =()=>{
+// const getRoles = async()=>{
 // 	const amdocsRoles = await fetch('www.emp-amdocs.com/admin/roles')
 // 							.then(res=>res.json())
 // 	return amdocsRoles;
 // }
 
-async function initAdvancedSearch=()=>{
-	return fetchAll(
-		const departments = await getDepartments();
-		const countries = await getCountries();
-		const workSites = await getWorkSites();
-		const roles = await getRoles();
-		)
-}
+// const initAdvancedSearch= async()=>{
+// 	return fetchAll(
+// 		const departments = await getDepartments();
+// 		const countries = await getCountries();
+// 		const workSites = await getWorkSites();
+// 		const roles = await getRoles();
+// 		)
+// }
 
 
 //TODO
